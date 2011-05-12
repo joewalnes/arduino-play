cf9611c7ca4625b69376571f7f01be4835af2871
Joe Walnes <jwalnes@drwholdings.com>
2011-05-12 07:58:03 -0500

    Add Arduino source to git

---
+++ lib/arduino/bootloaders/stk500v2/.dep/stk500boot.o.d
@@ -0,0 +1,70 @@
+stk500boot.o: stk500boot.c \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/inttypes.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/stdint.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/io.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/sfr_defs.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/iom1280.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/iomxx0_1.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/portpins.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/common.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/version.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/fuse.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/lock.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/interrupt.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/boot.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/eeprom.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/include/stddef.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/include-fixed/limits.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/pgmspace.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/util/delay.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/util/delay_basic.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/stdlib.h \
+  command.h \
+  /usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/math.h \
+  avrinterruptnames.h
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/inttypes.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/stdint.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/io.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/sfr_defs.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/iom1280.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/iomxx0_1.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/portpins.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/common.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/version.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/fuse.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/lock.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/interrupt.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/boot.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/eeprom.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/include/stddef.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/include-fixed/limits.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/avr/pgmspace.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/util/delay.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/util/delay_basic.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/stdlib.h:
+
+command.h:
+
+/usr/local/CrossPack-AVR-20100115/lib/gcc/avr/4.3.3/../../../../avr/include/math.h:
+
+avrinterruptnames.h:
