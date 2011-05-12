5c4c4b1b1640bc9b57c40569c11110b5bdf14fb8
Joe Walnes <jwalnes@drwholdings.com>
2011-05-12 07:52:27 -0500

    Use standard Arduino setup()/loop() instead of main()

--- blinky/blinky.cpp
+++ blinky/blinky.cpp
@@ -4,25 +4,21 @@ extern "C" void __cxa_pure_virtual(void) {
     while(1);
 } 
 
-int main() {
-	init();
+int led = HIGH;
 
+void setup() {
 	Serial.begin(9600);
   pinMode(13, OUTPUT);
-  int led = HIGH;
-
-	while (1) {
-		digitalWrite(13, (millis() / 500) % 2 ? HIGH : LOW);
-
-		if (Serial.available() > 0) {
-			led = led == HIGH ? LOW : HIGH;
-			//digitalWrite(13, led);
-			int b = Serial.read();
-			Serial.print("got: ");
-			Serial.println(b, DEC);
-		}
-	}
-
-  return 0;
 }
 
+void loop() {
+  digitalWrite(13, (millis() / 500) % 2 ? HIGH : LOW);
+
+  if (Serial.available() > 0) {
+    led = led == HIGH ? LOW : HIGH;
+    //digitalWrite(13, led);
+    int b = Serial.read();
+    Serial.print("got: ");
+    Serial.println(b, DEC);
+  }
+}
