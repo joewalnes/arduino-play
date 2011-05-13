9c1fc53760ea4d0e4638b487bd033db43136deea
Joe Walnes <jwalnes@drwholdings.com>
2011-05-13 18:05:26 -0500

    Moving out common build stuff

---
+++ common/arduino.mk
@@ -0,0 +1,5 @@
+CC       = avr-gcc
+CXX      = avr-g++
+CFLAGS   += -w -g -Os -ffunction-sections -fdata-sections -DF_CPU=$(F_CPU) -mmcu=$(MCU) -DARDUINO=22 -lm
+CXXFLAGS += $(CFLAGS) -fno-exceptions
+
--- common/device.mk
+++ common/device.mk
@@ -1,5 +1,14 @@
-F_CPU=16000000L
+# Arduino Mini Pro
+#F_CPU=16000000L
+#MCU=atmega328p
+#PORT=/dev/tty.usbserial*
+#BAUD=57600
+#PROTOCOL=stk500v1
+
+# Arduino Fio
+F_CPU=8000000L
 MCU=atmega328p
 PORT=/dev/tty.usbserial*
 BAUD=57600
 PROTOCOL=stk500v1
+
