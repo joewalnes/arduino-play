290971f00a6cb136c54f5a8a6b8ad9e5ab7f0748
Joe Walnes <jwalnes@drwholdings.com>
2011-05-13 18:29:53 -0500

    move more dup;

--- common/arduino.mk
+++ common/arduino.mk
@@ -8,3 +8,17 @@ CXXFLAGS += $(CFLAGS) -fno-exceptions
 
 %.elf:
 	$(CC) $(CFLAGS) -s -o $@ $^
+
+build/deps/arduinocore.a: $(wildcard ../lib/arduino/cores/arduino/*.c) $(wildcard ../lib/arduino/cores/arduino/*.cpp)
+	mkdir -p build/deps/arduinocore
+	cp -R $^ build/deps/arduinocore
+	ls build/deps/arduinocore/*.c   | sed -e 's/\.c/.o/'   | xargs make
+	ls build/deps/arduinocore/*.cpp | sed -e 's/\.cpp/.o/' | xargs make
+	(cd build/deps/arduinocore &&  avr-ar cq ../arduinocore.a *.o)
+
+build/deps/irremote.a: ../lib/IRremote/*.cpp
+	mkdir -p build/deps/irremote
+	cp -R $^ build/deps/irremote
+	ls build/deps/irremote/*.cpp | sed -e 's/.cpp/.o/' | xargs make
+	(cd build/deps/irremote &&  avr-ar cq ../irremote.a *.o)
+
