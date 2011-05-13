922181bd37ac35426e5e4e25f27fb571237676b2
Joe Walnes <jwalnes@drwholdings.com>
2011-05-13 18:19:53 -0500

    move more duplication into common make file

--- common/arduino.mk
+++ common/arduino.mk
@@ -3,3 +3,6 @@ CXX      = avr-g++
 CFLAGS   += -w -g -Os -ffunction-sections -fdata-sections -DF_CPU=$(F_CPU) -mmcu=$(MCU) -DARDUINO=22 -lm
 CXXFLAGS += $(CFLAGS) -fno-exceptions
 
+%.hex: %.elf
+	avr-objcopy -O ihex -R .eeprom $< $@
+
