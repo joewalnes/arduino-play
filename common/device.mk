1f978eadffb0478cd913b0eb5defe31f7f79ffd3
Joe Walnes <jwalnes@drwholdings.com>
2011-05-15 08:33:40 -0500

    Added LinkSprite DiamondBack device

--- common/device.mk
+++ common/device.mk
@@ -6,9 +6,17 @@
 #PROTOCOL=stk500v1
 
 # Arduino Fio
-F_CPU=8000000L
+#F_CPU=8000000L
+#MCU=atmega328p
+#PORT=/dev/tty.usbserial*
+#BAUD=57600
+#PROTOCOL=stk500v1
+
+# LinkSprite DiamondBack
+F_CPU=16000000L
 MCU=atmega328p
 PORT=/dev/tty.usbserial*
 BAUD=57600
 PROTOCOL=stk500v1
+
 
