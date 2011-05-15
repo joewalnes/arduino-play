e01a9e4adc36ef3421c51732ae8c4a5c892fa3b7
Joe Walnes <jwalnes@drwholdings.com>
2011-05-15 13:39:45 -0500

    Device now specified as variable, rather than having to update the makefile each time I switch device

--- common/device.mk
+++ common/device.mk
@@ -1,22 +1,35 @@
-# Arduino Mini Pro
-#F_CPU=16000000L
-#MCU=atmega328p
-#PORT=/dev/tty.usbserial*
-#BAUD=57600
-#PROTOCOL=stk500v1
+ALLDEVICES = mini fio diamondback
 
-# Arduino Fio
-#F_CPU=8000000L
-#MCU=atmega328p
-#PORT=/dev/tty.usbserial*
-#BAUD=57600
-#PROTOCOL=stk500v1
-
-# LinkSprite DiamondBack
+# DEVICE=mini (Arduino Mini Pro)
+AVAILABLE_DEVICES += mini
+ifeq ($(DEVICE),mini)
 F_CPU=16000000L
 MCU=atmega328p
 PORT=/dev/tty.usbserial*
 BAUD=57600
 PROTOCOL=stk500v1
+DEVICE_SET=true
+endif
 
+# DEVICE=fio (Arduino Fio)
+AVAILABLE_DEVICES += fio
+ifeq ($(DEVICE),fio)
+F_CPU=8000000L
+MCU=atmega328p
+PORT=/dev/tty.usbserial*
+BAUD=57600
+PROTOCOL=stk500v1
+DEVICE_SET=true
+endif
+
+# DEVICE=diamond (Link Sprite Diamondback)
+AVAILABLE_DEVICES += diamond
+ifeq ($(DEVICE),diamond)
+F_CPU=16000000L
+MCU=atmega328p
+PORT=/dev/tty.usbserial*
+BAUD=57600
+PROTOCOL=stk500v1
+DEVICE_SET=true
+endif
 
