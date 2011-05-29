d745036acbb976a42368854ff5642076417ea155
Joe Walnes <jwalnes@drwholdings.com>
2011-05-29 10:19:15 -0500

    Added more libs: EEPROM, SD, SPI, Ethernet, EthernetDHCP, EthernetDNS, EthernetBonjour

--- common/arduino.mk
+++ common/arduino.mk
@@ -66,4 +66,47 @@ build/deps/cmdmessenger.a: ../lib/CmdMessenger/*.cpp ../lib/CmdMessenger/*.h ../
 	ls build/deps/cmdmessenger/*.cpp | sed -e 's/.cpp/.o/' | xargs make INCLUDES=-Ibuild/deps/cmdmessenger
 	(cd build/deps/cmdmessenger &&  avr-ar cq ../cmdmessenger.a *.o)
 
+build/deps/spi.a: ../lib/SPI/*.cpp ../lib/SPI/*.h 
+	mkdir -p build/deps/spi
+	cp -R $^ build/deps/spi
+	ls build/deps/spi/*.cpp | sed -e 's/.cpp/.o/' | xargs make
+	(cd build/deps/spi &&  avr-ar cq ../spi.a *.o)
+
+build/deps/eeprom.a: ../lib/EEPROM/*.cpp ../lib/EEPROM/*.h 
+	mkdir -p build/deps/eeprom
+	cp -R $^ build/deps/eeprom
+	ls build/deps/eeprom/*.cpp | sed -e 's/.cpp/.o/' | xargs make
+	(cd build/deps/eeprom &&  avr-ar cq ../eeprom.a *.o)
+
+build/deps/sd.a: ../lib/SD/*.cpp ../lib/SD/*.h
+	mkdir -p build/deps/sd
+	cp -R $^ ../lib/SD/utility build/deps/sd
+	ls build/deps/sd/*.cpp | sed -e 's/.cpp/.o/' | xargs make INCLUDES=-Ibuild/deps/sd
+	(cd build/deps/sd &&  avr-ar cq ../sd.a *.o)
+
+build/deps/ethernet.a: ../lib/Ethernet/*.cpp ../lib/Ethernet/*.h ../lib/SPI/SPI.h
+	mkdir -p build/deps/ethernet
+	cp -R $^ ../lib/Ethernet/utility build/deps/ethernet
+	ls build/deps/ethernet/*.cpp | sed -e 's/.cpp/.o/' | xargs make INCLUDES="-Ibuild/deps/ethernet -Ibuild/deps/ethernet/utility"
+	(cd build/deps/ethernet &&  avr-ar cq ../ethernet.a *.o)
+
+build/deps/ethernetdhcp.a: ../lib/EthernetDHCP/*.cpp ../lib/EthernetDHCP/*.h
+	mkdir -p build/deps/ethernetdhcp
+	cp -R $^ ../lib/EthernetDHCP/utility build/deps/ethernetdhcp
+	ls build/deps/ethernetdhcp/*.cpp | sed -e 's/.cpp/.o/' | xargs make INCLUDES=-Ibuild/deps/ethernetdhcp
+	(cd build/deps/ethernetdhcp &&  avr-ar cq ../ethernetdhcp.a *.o)
+
+build/deps/ethernetdns.a: ../lib/EthernetDNS/*.cpp ../lib/EthernetDNS/*.h
+	mkdir -p build/deps/ethernetdns
+	cp -R $^ ../lib/EthernetDNS/utility build/deps/ethernetdns
+	ls build/deps/ethernetdns/*.cpp | sed -e 's/.cpp/.o/' | xargs make INCLUDES=-Ibuild/deps/ethernetdns
+	(cd build/deps/ethernetdns &&  avr-ar cq ../ethernetdns.a *.o)
+
+build/deps/ethernetbonjour.a: ../lib/EthernetBonjour/*.cpp ../lib/EthernetBonjour/*.h
+	mkdir -p build/deps/ethernetbonjour
+	cp -R $^ ../lib/EthernetBonjour/utility build/deps/ethernetbonjour
+	ls build/deps/ethernetbonjour/*.cpp | sed -e 's/.cpp/.o/' | xargs make INCLUDES=-Ibuild/deps/ethernetbonjour
+	(cd build/deps/ethernetbonjour &&  avr-ar cq ../ethernetbonjour.a *.o)
+
+
 
